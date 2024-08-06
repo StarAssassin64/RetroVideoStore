fb270257eb2e2c2c7eb6834c906b996b9a312d44
StarAssassin64 <starassassin64@starassassin64.com>
2024-08-06 09:57:15 -0500

    Added DbContext.cs

---
+++ Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetroVideoStore.Models;
+
+namespace RetroVideoStore.Data
+{
+    public class RentalContext : DbContext
+    {
+        public RentalContext(DbContextOptions<RentalContext> options) : base(options) { }
+
+        public DbSet<Rental> Rentals { get; set; }
+    }
+
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) {}
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
