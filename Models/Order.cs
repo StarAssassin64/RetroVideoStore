be81e6a18add8972fcace2522a6ad0ee7e408960
StarAssassin64 <starassassin64@starassassin64.com>
2024-08-06 09:44:39 -0500

    Added Rental and Order Models

---
+++ Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroVideoStore.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string CustomerEmail { get; set; }
+        [Required]
+        public Rental[] Rentals { get; set; }
+        public double TotalCost
+        { 
+            get 
+            { 
+                return TotalCost; 
+            } 
+            set 
+            {
+                TotalCost = 0;
+                foreach (Rental rental in Rentals) 
+                {
+                    TotalCost += rental.Cost;
+                } 
+            } 
+        }
+    }
+}
--- Models/Rental.cs
+++ Models/Rental.cs
@@ -1,7 +1,33 @@
-﻿namespace RetroVideoStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroVideoStore.Models
 {
     public class Rental
     {
-
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public string Format { get; set; }
+        public double Cost
+        {
+            get 
+            {
+                return Cost;
+            }
+            set
+            {
+                if (Format == "VHS")
+                { 
+                    Cost = 3.99; 
+                }
+                else if (Format == "DVD") 
+                { 
+                    Cost = 4.99; 
+                }
+            }
+        }
     }
 }
